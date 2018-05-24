d619ca2e98ff612f5d0835b0e307b642003022bf
hongxiao <hongtao.xiao@unb.ca>
2018-05-24 14:55:48 -0300

    append() and remove()

--- selection/selections.js
+++ selection/selections.js
@@ -1,2 +1,9 @@
 var first_child = d3.select('.child');
 first_child.text("first child");
+
+var parent = d3.selectAll('.parent');
+console.log(parent.attr('children'));
+
+parent.append('li');
+
+first_child.remove();
