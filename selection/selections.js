5b1272b136aa34e35c563ae6c0be000f011064b1
hongxiao <hongtao.xiao@unb.ca>
2018-05-25 16:36:00 -0300

    use attr() and style()

--- selection/selections.js
+++ selection/selections.js
@@ -2,9 +2,13 @@ var first_child = d3.select('.child');
 first_child.text("first child");
 
 var parent = d3.selectAll('.parent');
-console.log(parent.attr('children'));
-
+console.log(parent.attr('attr'));
 
 parent.insert('li', ':last-child');
 parent.append('li');
 // first_child.remove();
+
+parent.attr('attr', 4);
+console.log(parent.attr("attr"));
+parent.style('fill', 'red');
+console.log(parent.style('fill'));
