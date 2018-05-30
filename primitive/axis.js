6d38661da43c334e3f37cfbe11531f36e0ea2756
hongxiao <hongtao.xiao@unb.ca>
2018-05-30 16:13:05 -0300

    add logic of tickSize()

--- primitive/axis.js
+++ primitive/axis.js
@@ -1,2 +1,6 @@
 var axis = d3.axisTop(d3.scaleLinear());
 console.log(typeof(axis));
+
+console.log(axis.tickSize());
+axis.tickSizeOuter(5);
+console.log(axis.tickSize()); // this outputs .tickSizeInner()
