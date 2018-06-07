9ad2d72ce8cdc0a65378012fd996d618a89508f5
hongxiao <hongtao.xiao@unb.ca>
2018-06-07 16:07:52 -0300

    proper way to schedule a new transition

--- transition/add.js
+++ transition/add.js
@@ -1,5 +1,27 @@
+// this transition will be stopped
 d3.select("rect")
-  .style("fill", "orange")
   .transition()
-  // .duration(3000)  // default: 250 ms
-  .style("fill", "blue");
+  .duration(3000)
+  .style("fill", "red");
+
+// this transition wins
+d3.select("rect")
+  .transition()
+  .duration(3000)
+  .style("fill", "green");
+
+
+// add a new transition after an existing one
+d3.select("circle")
+  .transition()
+    .duration(3000)
+    .style("fill", "red")
+  .transition()
+    .duration(3000)
+    .style("fill", "green");
+
+// rect.transition()
+//   .duration(5000)
+//   .styleTween('fill', function() {
+//     return d3.interpolateRgb('green', 'black');
+//   });
