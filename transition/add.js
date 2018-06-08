bb39f2fd717c2a4bb56ca764be0e8094a35eba7f
hongxiao <hongtao.xiao@unb.ca>
2018-06-08 14:51:12 -0300

    working example of tweening

--- transition/add.js
+++ transition/add.js
@@ -1,27 +1,36 @@
-// this transition will be stopped
-d3.select("rect")
-  .transition()
-  .duration(3000)
-  .style("fill", "red");
 
-// this transition wins
-d3.select("rect")
-  .transition()
-  .duration(3000)
-  .style("fill", "green");
-
-
-// add a new transition after an existing one
-d3.select("circle")
-  .transition()
+if (false) {
+  // this transition will be stopped
+  d3.select("rect")
+    .transition()
     .duration(3000)
-    .style("fill", "red")
-  .transition()
+    .style("fill", "red");
+
+  // this transition wins
+  d3.select("rect")
+    .transition()
     .duration(3000)
     .style("fill", "green");
+}
 
-// rect.transition()
-//   .duration(5000)
-//   .styleTween('fill', function() {
-//     return d3.interpolateRgb('green', 'black');
-//   });
+if (false) {
+  // add a new transition after an existing one
+  d3.select("circle")
+    .transition()
+    .duration(3000)
+    .style("fill", "red")
+    .transition()
+    .duration(3000)
+    .style("fill", "green");
+}
+
+if (true) {
+  // use tweening
+  d3.select("ellipse")
+    .style('fill', 'red')
+    .transition()
+    .duration(3000)
+    .styleTween('fill', function () {
+      return d3.interpolateRgb('green', 'black');
+    });
+}
