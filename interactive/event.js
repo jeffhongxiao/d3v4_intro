7a857fa7f88d33c017046984ee561ee6282f0ef0
hongxiao <hongtao.xiao@unb.ca>
2018-06-13 15:50:50 -0300

    add double click event

--- interactive/event.js
+++ interactive/event.js
@@ -2,6 +2,9 @@ d3.select('rect')
   .on('click', function() {
     console.log('clicked');
   })
+  .on('dblclick', function() {
+    console.log('double clicked');
+  });
 
 // remove the listener to an event type
 // d3.select('rect').on('click', null);
