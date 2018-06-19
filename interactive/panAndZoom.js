33d5b784d4a76bd67ac2a339ef13fe5de31c90ef
hongxiao <hongtao.xiao@unb.ca>
2018-06-19 11:02:19 -0300

    working example of panAndZoom.js

--- interactive/panAndZoom.js
+++ interactive/panAndZoom.js
@@ -1,10 +1,17 @@
 var svg = d3.select('svg');
-var g = svg.select('g');
+
+var circle = svg.append('circle')
+    .attr('cx', 20)
+    .attr('cy', 20)
+    .attr('r', 50)
+    .style('fill', 'tan');
 
 var zoomHandler = d3.zoom()
   .on("zoom", function () {
-    g.attr("transform", d3.event.transform);
+    circle.attr("transform", d3.event.transform);
   });
 
-g.call(zoomHandler);
-
+// MUST BE: svg
+// CANNOT BE: circle
+svg.call(zoomHandler);
+// zoomHandler(svg);
